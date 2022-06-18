e01b7c4964735cc361f5f1082a2e00784e6d18f1
4hsnim <4hsnim@naver.com>
2022-06-18 17:48:07 +0900

    Signup Page 틀만 완성(padding이나 maring같은거 안줌)

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
+import SignUp from '../components/signup/SignUp';
 
 const SignUpPage = () => {
    return (
       <>
-         {/* <SignUp> */}
-            <BtnSignUpTxt>가입하기</BtnSignUpTxt>
-         {/* </SignUp> */}
+         <SignUp/>
       </>
    );
 };
 
-const BtnSignUpTxt = styled.span`
-   box-sizing: border-box;
-   margin: 0;
-   padding: 0;
-   letter-spacing: -0.6px;
-   font-size: 15px;
-   color: #5f0080;
-`;
+
 
 export default SignUpPage;
